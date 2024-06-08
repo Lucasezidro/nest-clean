32f1d285b5aaeeb9be8cde44759a043b75af3882
lucasezidro <lucasezidro7@gmail.com>
2024-06-08 10:37:56 -0300

    create comment on question controller

--- src/domain/forum/application/use-cases/comment-on-question.ts
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -4,6 +4,7 @@ import { QuestionCommentsRepository } from '../repositories/question-comments-re
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found'
+import { Injectable } from '@nestjs/common'
 
 interface CommentOnQuestionUseCaseRequest {
   authorId: string
@@ -18,6 +19,7 @@ type CommentOnQuestionUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class CommentOnQuestionUseCase {
   constructor(
     private questionRepository: QuestionRepository,
