c4c78dac233d37488cabc31335cc3180a0795719
lucasezidro <lucasezidro7@gmail.com>
2024-06-11 11:34:39 -0300

    send notification when an answer or question is created or saved

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -7,6 +7,7 @@ import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
 import { QuestionAttachmentRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
 import { PrismaQuestionDetailsMapper } from '../mappers/prisma-question-details-mapper'
+import { DomainEvents } from '@/core/events/domain-events'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionRepository {
@@ -93,6 +94,8 @@ export class PrismaQuestionsRepository implements QuestionRepository {
         question.attachments.getRemovedItems(),
       ),
     ])
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async create(question: Question): Promise<void> {
@@ -105,6 +108,8 @@ export class PrismaQuestionsRepository implements QuestionRepository {
     await this.questionAttachmentRepository.createMany(
       question.attachments.getItems(),
     )
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question): Promise<void> {
